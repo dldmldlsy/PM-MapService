5f039e95eccd70855629ec8545ed3c74a4e0f7fb
dldmldlsy <dldmldlsy@naver.com>
2022-10-22 21:30:25 +0900

    프로젝트 생성

---
+++ src/main/java/com/capstone/team5/pmmap/PmMapApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.team5.pmmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/team5/pmmap/PmMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.team5.pmmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
